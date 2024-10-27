74d015309853393b98c873dee2c0600d49534c7d
fatybiry <fatimazahra.biry@gmail.com>
2024-10-27 21:46:06 +0100

    Add files via upload

---
+++ forms/contact.php
@@ -0,0 +1,552 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Personal Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+<!-- =======================================================
+  * Template Name: Personal
+  * Updated: Sep 18 2023 with Bootstrap v5.3.2
+  * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ===============================================
+
+  ========= -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container">
+
+      <h1><a href="index.html">Benchelha Nouhaila</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="mr-auto"><img src="niha.jpeg" alt="" class="img-fluid"></a> -->
+      <h2>Elève ingénieur <span>en topographie</span>  à l'IAV HASSAN 2</h2>
+      <img src="//fatybiry.github.io/pho/photo.png" alt="Description de l'image" class="content-image">
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="#header">Acceuil</a></li>
+          <li><a class="nav-link" href="#about">A propos de moi</a></li>
+          <li><a class="nav-link" href="#resume">Expérience</a></li>
+	  <li><a class="nav-link" href="#Volontariat">Volontariat</a></li>
+          <li><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+
+    <!-- ======= About Me ======= -->
+    <div class="about-me container">
+
+      <div class="section-title">
+        <h2>A propos de moi</h2><br>
+        <p></p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="assets/img/bn.jpg" class="img-fluid" alt=""style="margin-left: 80px;">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>Ingénieur géomètre topographe</h3>
+ 
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Date de naissance:</strong> <span>28 Février 2002</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>E-mail:</strong> <span>benchelhanouhaila@iav.ac.ma</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Tél:</strong> <span>+212668038393</span></li>
+           
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>21</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Statut:</strong>élève ingénieur <span></span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Ville:</strong> <span>Kelaa des sraghna,Maroc</span></li>
+                
+              </ul>
+            </div>
+          </div>
+          <p>
+             Je suis en troisième année du cycle ingénieur dans la filière de science géomatique et ingénierie topographique a l'iav Hassan 2, et actuellement en recherche d'un stage PFE.
+          </p>
+        </div>
+      </div>
+
+    </div><!-- End About Me -->
+
+    </div><!-- End Counts -->
+
+    <!-- ======= LOGITIELS  ======= -->
+    <div class="skills container">
+
+      <div class="section-title">
+        <h2>LOGITIELS</h2>
+      </div>
+
+      <div class="row skills-content">
+
+        <div class="col-lg-6">
+
+          <div class="progress">
+            <span class="skill">AUTOCAD/MICROSTATION<i class="val">90%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">TOPOGEN / COVADIS<i class="val">90%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">QGIS / ARCGIS <i class="val">75%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-lg-6">
+
+          <div class="progress">
+            <span class="skill">ECOGNITION / ENVI<i class="val">60%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">TBC <i class="val">80%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">ERDAS <i class="val">85%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div><!-- End Skills -->
+
+    <!-- ======= LANGUES  ======= -->
+    <div class="skills container">
+
+      <div class="section-title">
+        <h2>LANGUES</h2>
+      </div>
+
+      <div class="row skills-content">
+
+        <div class="col-lg-6">
+
+          <div class="progress">
+            <span class="skill">ARABE ( langue maternelle )<i class="val">95%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">FRANCAIS<i class="val">90%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">ANGLAIS <i class="val">75%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+        </div>
+
+
+
+
+
+
+</div>
+
+
+    </div><!-- End Skills -->
+
+
+    <!-- ======= Interests ======= -->
+    <div class="interests container">
+
+      <div class="section-title">
+        <h2>CENTRES D'INTERET</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-3 col-md-4">
+          <div class="icon-box">
+          
+            <h3>Sport</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+          <div class="icon-box">
+          
+            <h3>Photographie</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4">
+          <div class="icon-box">
+           
+            <h3>Lecture</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+          <div class="icon-box">
+            
+            <h3>Design</h3>
+          </div>
+        </div>
+      </div>
+
+    </div><!-- End Interests -->
+
+    <!-- ======= Testimonials ======= -->
+    <div class="testimonials container">
+
+      <div class="section-title">
+        <h2>Compétences</h2>
+      </div>
+
+      <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+        <div class="swiper-wrapper">
+
+          <div class="swiper-slide">
+            <div class="testimonial-item">
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Capable d'établir des relations interpersonnelles positives et de collaborer efficacement avec les personnes de mon entourage.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+              <h3>Sens du relationnel</h3>
+              <h4></h4>
+            </div>
+          </div><!-- End testimonial item -->
+
+     
+
+          <div class="swiper-slide">
+            <div class="testimonial-item">
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+               Participation active à de nombreux projets d'équipe, démontrant ma capacité à travailler harmonieusement avec des collègues aux compétences diverses.
+         
+              </p>
+              <h3>Travail en équipe</h3>
+              <h4></h4>
+            </div>
+          </div><!-- End testimonial item -->
+
+          <div class="swiper-slide">
+            <div class="testimonial-item">
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Fiabilité en matière de ponctualité, garantissant une présence constante et à l'heure pour les réunions, les projets et les tâches.
+                <i class="bx bxs-quote-alt-right quote-icon-right"></i>
+              </p>
+              <h3>Ponctualité</h3>
+              <h4></h4>
+            </div>
+          </div><!-- End testimonial item -->
+
+          <div class="swiper-slide">
+            <div class="testimonial-item">
+              <p>
+                <i class="bx bxs-quote-alt-left quote-icon-left"></i>
+                Réalisation de montages vidéo pour divers types de projets, intégrant des éléments de conception pour une présentation visuellement attrayante.
+                
+              </p>
+           
+              <h3>Montage vidéo</h3>
+              <h4></h4>
+            </div>
+          </div><!-- End testimonial item -->
+
+        </div>
+        <div class="swiper-pagination"></div>
+      </div>
+
+      <div class="owl-carousel testimonials-carousel">
+
+      </div>
+
+    </div><!-- End Testimonials  -->
+
+  </section><!-- End About Section -->
+
+  <!-- ======= Resume Section ======= -->
+  <section id="resume" class="resume">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Expérience</h2>
+        <p></p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6">
+   
+
+          <h3 class="resume-title">Education</h3>
+
+          <div class="resume-item">
+            <h4>BACCALAUREAT</h4>
+            <h5>2018 - 2019</h5>
+            <p><em>Nakhil El Bouchra, El kelaa Des sraghna</em></p>
+            <p>Filière du bac : Science mathématiques , Mention Très Bien</p>
+          </div>
+
+          <div class="resume-item">
+            <h4>APESA</h4>
+            <h5>2019 - 2020</h5>
+            <p><em>Année préparatoire aux études supérieur en agriculture, Rabat</em></p>
+            <p></p>
+          </div>
+
+          <div class="resume-item">
+            <h4>IAV</h4>
+            <h5>2020 - 2024</h5>
+            <p><em>Institut Agronomique et vétérinaire Hassan 2, Rabat</em></p>
+            <p>Filière : Science géomatique et ingénierie topographique</p>
+          </div>
+
+        </div>
+        <div class="col-lg-6">
+          <h3 class="resume-title">Stages</h3>
+          <div class="resume-item">
+            <h4>Stage Cadastrale</h4>
+            <h5>Juillet 2022</h5>
+            <p><em>ANCFCC,EL Kelaa des sraghna</em></p>
+            <p>
+            <ul>
+              <li>2 semaines au cadastre et une semaine a la conservation foncière</li>
+              
+            </ul>
+            </p>
+          </div>
+          <div class="resume-item">
+            <h4>Stage de professionnalisation</h4>
+            <h5>Mai 2023</h5>
+            <p><em>vDépartement de topographie et photogrammétrie de l'IAV HASSAN 2</em></p>
+            <p>
+            <ul>
+              <li>Phase Géodésie</li>
+              <li>Phase Topographie</li>
+              <li>Phase Photogrammétrie</li>
+    
+            </ul>
+            </p>
+          </div>
+
+          <div class="resume-item">
+            <h4>Stage d'entreprise</h4>
+            <h5>Septembre 2023</h5>
+            <p><em>Afric topo ingénierie</em></p>
+            <p>
+            <ul>
+              <li>Découvrir le monde de l’entreprise.</li>
+              <li>Mettre en pratique les connaissances théoriques acquises et les confronter aux défis et situations réelles du terrain.</li>
+              <li>Développer un réseau professionnel qui pourrait s'avérer précieux en cas de besoin d'opportunités professionnelles.</li>
+    
+            </ul>
+            </p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End Resume Section -->
+
+  <!-- ======= Volontariat Section ======= -->
+  <section id="Volontariat" class="Volontariat">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Volontariat</h2>
+        
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+          <div class="icon-box">
+            
+            <h4><a href="">YED*YED</a></h4>
+            <p>Vice présidente de l'association YED*YED</p>
+          </div>
+        </div>  <br>
+
+        <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+          <div class="icon-box">
+            
+            <h4><a href="">AMETOP</a></h4>
+            <p>Membre de l'AMETOP depuis 2020</p>
+          </div>
+        </div>
+
+        
+
+      </div>
+
+    </div>
+  </section><!-- End Volontariat Section -->
+
+
+ 
+
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="contact">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Contact</h2>
+        <p></p>
+      </div>
+
+      <div class="row mt-2">
+
+        <div class="col-md-6 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-map"></i>
+            <h3>Mon Adresse</h3>
+            <p>N110,Nour2,El kelaa des sraghna</p>
+          </div>
+        </div>
+
+        <div class="col-md-6 mt-4 mt-md-0 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-share-alt"></i>
+            <h3>Social Profiles</h3>
+            <div class="social-links">
+	      <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>	
+              <a href=" https://instagram.com/_nouhaila_ben_?igshid=MzRlODBiNWFlZA=="> <font color="white"size="2"> nouhaila_ben</font> </a>
+              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+	      <a href="https://www.facebook.com/nouhaila.ben.754703?mibextid=ZbWKwL"> <font color="white"size="2"> Nouhaila Ben</font> </a>
+              
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-envelope"></i>
+            <h3>Email Me</h3>
+            <p>benchelhanouhaila@iav.ac.ma</p>
+          </div>
+        </div>
+        <div class="col-md-6 mt-4 d-flex align-items-stretch">
+          <div class="info-box">
+            <i class="bx bx-phone-call"></i>
+            <h3>Call Me</h3>
+            <p>+212668038393</p>
+          </div>
+        </div>
+      </div>
+
+      <form action="forms/contact.php" method="post" role="form" class="php-email-form mt-4">
+        <div class="row">
+          <div class="col-md-6 form-group">
+            <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+          </div>
+          <div class="col-md-6 form-group mt-3 mt-md-0">
+            <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+          </div>
+        </div>
+        <div class="form-group mt-3">
+          <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+        </div>
+        <div class="form-group mt-3">
+          <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+        </div>
+        <div class="my-3">
+          <div class="loading">Loading</div>
+          <div class="error-message"></div>
+          <div class="sent-message">Your message has been sent. Thank you!</div>
+        </div>
+        <div class="text-center"><button type="submit">Send Message</button></div>
+      </form>
+
+    </div>
+  </section><!-- End Contact Section -->
+
+  <div class="credits">
+    <!-- All the links in the footer should remain intact. -->
+    <!-- You can delete the links only if you purchased the pro version. -->
+    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/personal-free-Expérience-bootstrap-template/ -->
+    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
